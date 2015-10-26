35ea932d3be790262c6b0751d206258e3efddcae
Florent Rougon <f.rougon@free.fr>
2015-10-27 00:09:31 +0100

    InfoWindow: support using a StringVar for the label text
    * ffgo/gui/infowindow.py (InfoWindow.__init__): new 'textvariable'
    keyword argument passed to the Label constructor if the 'text' argument
    is None. Change the order of the arguments so that 'text' comes right
    after 'self'. This is because the 'text' argument is more likely to be
    used alone than 'title'.

--- ffgo/gui/infowindow.py
+++ ffgo/gui/infowindow.py
@@ -13,19 +13,24 @@ from ..constants import PROGNAME
 # useful to study.
 class InfoWindow(Toplevel):
 
-    def __init__(self, master=None, title=None, text=None,
+    def __init__(self, master=None, text=None, textvariable=None, title=None,
                  font=tkinter.font.nametofont("TkHeadingFont"), **kw):
         Toplevel.__init__(self, master, **kw)
-        self._window(master, title, text, font)
+        self._window(master, title, text, textvariable, font)
 
-    def _window(self, master, title, text, font):
+    def _window(self, master, title, text, textvariable, font):
         title = title if title is not None else _("{prg}").format(prg=PROGNAME)
         self.title(title)
         self.protocol("WM_DELETE_WINDOW", self._doNotQuit)
         self.resizable(width=False, height=False)
         self.transient(master)
 
-        self.label = Label(self, borderwidth=20, text=text, font=font)
+        if text is not None:
+            kwargs = {"text": text}
+        else:
+            kwargs = {"textvariable": textvariable}
+
+        self.label = Label(self, borderwidth=20, font=font, **kwargs)
         self.label.pack()
         self.update_idletasks()
         self.grab_set()         # make the dialog modal
