frougon/FFGo

commit 42a83f994f26c55dc1dcef1b192b5831b54feacb

Don't ignore IOError in Config._readApt()
* If the file doesn't exist, it will be created. If there is an error
reading it, this is serious and should not be silenced.

ffgo/config.py
@@ -755,43 +755,38 @@ configurations are kept separate.""")
         information about the same airport at given index.
 
         """
-        try:
-            # Make new file.
-            if not os.path.exists(APT):
-                self._makeApt()
+        if not os.path.exists(APT):
+            self._makeApt()     # create a new file
 
-            icao, name, rwy = [], [], []
-            logger.info("Opening apt file '{}' for reading".format(APT))
+        icao, name, rwy = [], [], []
+        logger.info("Opening apt file '{}' for reading".format(APT))
 
-            with open(APT, "r", encoding="utf-8") as fin:
-                if self.filteredAptList.get():
-                    installed_apt = self._readInstalledAptList()
-                    for line in fin:
-                        line = line.strip().split('=')
-                        if line[0] in installed_apt:
-                            installed_apt.remove(line[0])
-                            icao.append(line[0])
-                            name.append(line[1])
-                            rwy_list = []
-                            for i in line[2:-1]:
-                                rwy_list.append(i)
-                            rwy_list.sort()
-                            rwy.append(rwy_list)
-
-                else:
-                    for line in fin:
-                        line = line.strip().split('=')
+        with open(APT, "r", encoding="utf-8") as fin:
+            if self.filteredAptList.get():
+                installed_apt = self._readInstalledAptList()
+                for line in fin:
+                    line = line.strip().split('=')
+                    if line[0] in installed_apt:
+                        installed_apt.remove(line[0])
                         icao.append(line[0])
                         name.append(line[1])
                         rwy_list = []
                         for i in line[2:-1]:
                             rwy_list.append(i)
+                        rwy_list.sort()
                         rwy.append(rwy_list)
 
-            return icao, name, rwy
+            else:
+                for line in fin:
+                    line = line.strip().split('=')
+                    icao.append(line[0])
+                    name.append(line[1])
+                    rwy_list = []
+                    for i in line[2:-1]:
+                        rwy_list.append(i)
+                    rwy.append(rwy_list)
 
-        except IOError:
-            return [], [], []
+        return (icao, name, rwy)
 
     def _readInstalledAptList(self):
         """Read locally installed airport list.

commit 986952572b72cffec4962fc3caffc92d027a6b70

Don't fail in Config._readApt() when the config dir is empty
* ffgo/config.py (Config._readApt): fix problem introduced in commit
42a83f994f26c55dc1dcef1b192b5831b54feacb: if Config.FG_root is empty,
then Config._makeApt() does not create the 'apt' file. Therefore, even
after this call, one must check the existence of 'apt' before trying to
read it.

ffgo/config.py
@@ -769,9 +769,13 @@ configurations are kept separate.""")
 
         """
         if not os.path.exists(APT):
-            self._makeApt()     # create a new file
+            # Create a new file if self.FG_root is non-empty
+            self._makeApt()
 
         icao, name, rwy = [], [], []
+        if not os.path.isfile(APT): # may happen if self.FG_root was empty
+            return (icao, name, rwy)
+
         logger.info("Opening apt file '{}' for reading".format(APT))
 
         with open(APT, "r", encoding="utf-8") as fin: