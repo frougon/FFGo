0898feec1b3bf14187102375be8c273e24c7c5fd
Florent Rougon <f.rougon@free.fr>
2016-03-19 22:48:38 +0100

    Release 1.11.1

--- ffgo/version.py
+++ ffgo/version.py
@@ -25,7 +25,7 @@ class VersionInfo(_VersionInfo):
     def __repr__(self):
         return "{0}.{1}".format(__name__, _VersionInfo.__repr__(self))
 
-version_info = VersionInfo(1, 11, 0, "")
+version_info = VersionInfo(1, 11, 1, "")
 __version__ = str(version_info)
 
 del collections
