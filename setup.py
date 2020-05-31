c83d2331354641dd2db8fdacf4f3572c662818ba
Florent Rougon <f.rougon@free.fr>
2020-05-31 10:18:10 +0200

    Refresh setup.py and docs/INSTALL/INSTALL_en

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2015, 2016  Florent Rougon
+# Copyright (C) 2015-2020  Florent Rougon
 #
 # This file is distributed under the terms of the DO WHAT THE FUCK YOU WANT TO
 # PUBLIC LICENSE version 2, dated December 2004, by Sam Hocevar. You should
@@ -35,15 +35,29 @@ def do_setup():
         description="A powerful graphical launcher for the FlightGear "
                     "flight simulator",
         long_description=long_description,
+        long_description_content_type="text/x-rst",
+
         url="http://frougon.net/projects/{}/".format(setuptools_pkg),
         license='WTFPLv2',
+        project_urls={
+          "Screenshots": "http://frougon.net/projects/FFGo/gallery/",
+          "FFGo doc. on the FlightGear wiki":
+            "http://wiki.flightgear.org/FFGo",
+          "FFGo conditional config doc.":
+            "http://frougon.net/projects/FFGo/doc/README-conditional-config/",
+          "Git repository": "https://github.com/frougon/FFGo",
+          "Issue tracker": "https://github.com/frougon/FFGo/issues",
+        },
 
-        author="Robert Leda, Florent Rougon",
-        author_email=\
-         'https://sites.google.com/site/erobosprojects/flightgear/add-ons/fgo, '
-         'f.rougon@free.fr',
-        maintainer="Florent Rougon (fork author)",
-        maintainer_email='f.rougon@free.fr',
+        # According to
+        # <https://packaging.python.org/specifications/core-metadata/> and the
+        # rendering on PyPI, it appears that only the original author can be
+        # listed in the 'author' field. FGo! and its author Robert Leda are
+        # mentioned at the very beginning of README.rst, and therefore also of
+        # the FFGo home page. Given the limitations for the 'author' field, I
+        # believe this is reasonable and fair.
+        author="Florent Rougon",
+        author_email="f.rougon@free.fr",
 
         # See <https://pypi.org/pypi?%3Aaction=list_classifiers>
         classifiers=[
@@ -52,12 +66,10 @@ def do_setup():
             'License :: DFSG approved',
             'Operating System :: OS Independent',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
             'Topic :: Games/Entertainment :: Simulation',
         ],
         keywords=\
-            'FlightGear flightgear flight simulator launcher FFGo FGo!',
+            'FlightGear,flightgear,flight,simulator,launcher,FFGo,FGo!',
         packages=[pypkg_name],
         include_package_data=True,
         # Files to exclude from installation
@@ -66,6 +78,7 @@ def do_setup():
                                   '*.xcf', '*/thumbnail-no-Pillow.svg',
                                   '*/Makefile', '*/Makefile.py-functions'] },
 
+        python_requires='~=3.4',
         install_requires=['CondConfigParser'],
         extras_require = {'images':  ['Pillow'], 'geo': ['geographiclib']},
         entry_points={'console_scripts': ['ffgo = ffgo.main:main'],
